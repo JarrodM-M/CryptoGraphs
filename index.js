2426b6f00d65ddf56ae6bab4a6f06687269808bb
JarrodM-M <85252030+JarrodM-M@users.noreply.github.com>
2021-06-15 14:35:06 -0230

    api + graphs

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-let x = document.getElementById("title");
+            async function getBTC(){
+                const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=cad");
+                const data = await response.json();
+                const { cad } = data.bitcoin;
 
-console.log(x);
+                document.getElementById('btc').textContent = cad;
+                console.log(cad);
+            }
+            getBTC();
+
+            async function getNANO(){
+                const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=nano&vs_currencies=cad");
+                const data = await response.json();
+                const { cad } = data.nano
+               
+                document.getElementById("nano").textContent = cad;
+                console.log(cad);
+            }
+            getNANO();
+
+            async function getETH(){
+                const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=cad");
+                const data = await response.json();
+                const { cad } = data.ethereum;
+
+                document.getElementById("eth").textContent = cad
+                console.log(cad);
+            }
+            getETH();
+
+            async function getSOL(){
+                const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=cad");
+                const data = await response.json();
+                const { cad } = data.solana;
+                
+                document.getElementById("sol").textContent = cad
+                console.log(cad);
+            }
+            getSOL();
+
+
