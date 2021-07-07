b63995d8d79fa993970be9cf4fdc6498d406871d
JarrodM-M <85252030+JarrodM-M@users.noreply.github.com>
2021-07-07 13:50:19 -0230

    Js fetch updates

--- index.js
+++ index.js
@@ -9,7 +9,9 @@
             
             console.log(link);
             
-            let coins = ["bitcoin", "ethereum", "nano"]; 
+            let coins = ["bitcoin", "ethereum", "nano"];
+            let currencies = ["cad", "usd"];
+            let intervals = ["hourly", "daily", "monthly", 'yearly']; 
 
             for(let i=0; i<coins.length; i++){
                 console.log("https://api.coingecko.com/api/v3/coins/" + coins[i] + "/market_chart?vs_currency=" + currency + days + interval);
@@ -28,7 +30,7 @@
             async function getNANO(){
                 const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=nano&vs_currencies=cad");
                 const data = await response.json();
-                const { cad } = data.nano
+                const { cad } = data.nano;
                
                 document.getElementById("nano").textContent = cad;
                 console.log(cad);
@@ -40,7 +42,7 @@
                 const data = await response.json();
                 const { cad } = data.ethereum;
 
-                document.getElementById("eth").textContent = cad
+                document.getElementById("eth").textContent = cad;
                 console.log(cad);
             }
             getETH();
@@ -58,52 +60,12 @@
             async function graphdatamarketprice(){
                 const response = await fetch (link);
                 const data = await response.json();
-                const { marketprice } = data.prices;
-
-                document.getElementById("price").textContent = marketprice;
-                console.log( data.market_caps );
-                console.log( data.prices);
+                
+                console.log(data.prices[24]);
             }
             graphdatamarketprice();
 
 
-const ctx = document.getElementById('chart').getContext('2d');
-const xlabels = [];
-const mychart  = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: xlabels,
-        datasets: [
-            {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
 
 
 
