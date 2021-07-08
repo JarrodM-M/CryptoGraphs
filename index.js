ae552b8587bcd5333c4fa4b9fea7b94ac7fd71e5
JarrodM-M <85252030+JarrodM-M@users.noreply.github.com>
2021-07-08 13:32:13 -0230

    tests

--- index.js
+++ index.js
@@ -13,30 +13,100 @@
             
             const xtime = [];
             const yprice = [];
-            console.log(xtime, yprice);
+            const unixlink = [];
+            
+            
 
 // maybe foreachloop to get xtime into the timestamp of unixapi link
-            let unixapi = "https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp=" 
-           
- 
-// --------------------------------------------------------------------------------------------
+            let unixapi = "https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp="
+
+            
+            async function realtime(){
+                await graphdatamarketprice();
+                xtime.forEach(unixcode => {
+                    const fulllink = unixapi + unixcode;
+                    unixlink.push(fulllink);
+                    
+                });
+            };
+            
+            
+            
+
+            async function test(){
+                await realtime();
+                unixlink.forEach(timefetch => {
+                    const truetime = timefetch.length-3
+                    console.log(truetime)
+                    
+
+                });
+            
+            };
+            test();
+            
+
+
+            async function graphdatamarketprice(){
+                const response = await fetch (coingeckolink);
+                const data = await response.json();
+                const table = data.prices;
+                table.forEach(tnp => {
+                   const time = tnp[0];
+                   xtime.push(time); 
+                   const price = tnp[1];
+                   yprice.push(price);
+                });
+             };
+            
+
+             async function cryptochart(){
+                await graphdatamarketprice();
+                const ctx = document.getElementById('myChart');
+                const myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: truetime,
+                        datasets: [{
+                            label: 'Prices',
+                            data: yprice,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                            ],
+                            borderWidth: 1
+                        }]
+                    },    
+                });
+            }
+            
+
+            // --------------------------------------------------------------------------------------------
+
 // Figure out what makes most sense. For loops or foreach being pushed then grabbed and inserted into masterlink object to use in generating graph data.   
 // Foreach "tunnel" works but it doesn't seem to make sense to use,even though it will generate all possibilities. 
 // 
 
 coins.forEach(coinlist => {
-                currencies.forEach(currencylist => {
-                    console.log("https://api.coingecko.com/api/v3/coins/" + coinlist + "/market_chart?vs_currency=" + currencylist + days + interval)
-                    });
-            });
+    currencies.forEach(currencylist => {
+        console.log("https://api.coingecko.com/api/v3/coins/" + coinlist + "/market_chart?vs_currency=" + currencylist + days + interval)
+        });
+});
+
+
+for(let i=0; i<coins.length; i++){
+    console.log("https://api.coingecko.com/api/v3/coins/" + coins[i] + "/market_chart?vs_currency=" + currency + days + interval);
+}
+
+
+
+
+            // -----------------------------------------------------------------------------------------------------------------------------------
+            
 
             
-            for(let i=0; i<coins.length; i++){
-                console.log("https://api.coingecko.com/api/v3/coins/" + coins[i] + "/market_chart?vs_currency=" + currency + days + interval);
-            }
-//---------------------------------------------------------------------------------------------
-           
-           
             async function getBTC(){
                 const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=cad");
                 const data = await response.json();
@@ -76,44 +146,8 @@ coins.forEach(coinlist => {
                 console.log(cad);
             }
             getSOL();
-            
-            
-            async function graphdatamarketprice(){
-                const response = await fetch (coingeckolink);
-                const data = await response.json();
-                const table = data.prices;
-                table.forEach(tnp => {
-                   const time = tnp[0];
-                   xtime.push(time); 
-                   const price = tnp[1];
-                   yprice.push(price);
-                   console.log(time, price)
-                });
-             };
-            
 
-             async function cryptochart(){
-                await graphdatamarketprice();
-                const ctx = document.getElementById('myChart');
-                const myChart = new Chart(ctx, {
-                    type: 'line',
-                    data: {
-                        labels: xtime,
-                        datasets: [{
-                            label: 'Prices',
-                            data: yprice,
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                            ],
-                            borderWidth: 1
-                        }]
-                    },    
-                });
-            }
-            cryptochart();
+            
             
             
 
